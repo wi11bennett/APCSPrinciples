139889acac5dcd2906cd607d90b9698cc3d0deca
noahclementboys <57371254+noahclementboys@users.noreply.github.com>
2019-11-06 11:15:34 -0800

    Add files via upload

--- GUESSINgGame.py
+++ GUESSINgGame.py
@@ -2,6 +2,8 @@
 import random
 rnum = random.randint (1000, 9999)
 print (rnum)
+#guesses vriable
+guesses = 5
 
 strnum = str (rnum)
 rnumlist = []
@@ -11,19 +13,23 @@ for digit in strnum:
 print (rnumlist)
 
 #nothing for now
-userinput1 = input ("Enter 4 numbers: ")
-if int(userinput1) == rnumlist:
-    print ("correct")
+while guesses != 0:
+    userinput1 = input ("Enter 4 numbers: ")
 
-for digit in userinput1 :
-    guesslist.append(int(digit))
+    for digit in userinput1 :
+        guesslist.append(int(digit))
+    if guesslist == rnumlist:
+        print("Congrats")
+        guesses = 0
 
-for i in rnumlist:
-    for j in guesslist:
-        if i == j :
-            print (rnumlist.index(i))
-        else:
-            print ("incorrect")
+    else:
+        for i in rnumlist:
+            for j in guesslist:
+                if i == j :
+                    print (rnumlist.index(i))
+                else:
+                    print ("incorrect")
+
 
 
 
